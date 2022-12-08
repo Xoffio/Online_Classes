1986c5e6588fb9654af97e6a6de9f3215727458f
xoffio <marquessricardo@gmail.com>
2022-12-08 14:45:37 -0500

    Gradient Descent added.

--- machineLearning/week2/rust_ex1/src/linear_regression.rs
+++ machineLearning/week2/rust_ex1/src/linear_regression.rs
@@ -9,7 +9,32 @@ pub fn compute_cost(xy: &Array2<f64>, theta: &Array1<f64>) -> f64 {
     let y = xy.slice(s![.., 1]);
 
     let cost = ((x.dot(theta)-y).mapv(|i| i.powf(2.0))).sum() / (2.0*(x_num_of_cols as f64));
-    println!("{:?}", cost  );
+    //println!("{:?}", cost  );
 
+    // Return the cost
     cost
+}
+
+pub fn gradient_descent(xy: &Array2<f64>, theta: &mut Array1<f64>, alpha: f64, iterations: usize){
+    let x_num_of_cols = xy.dim().0;
+    let x = stack!(
+        Axis(1), 
+        Array1::<f64>::ones(x_num_of_cols), 
+        xy.slice(s![.., 0]));
+    let y = xy.slice(s![.., 1]);
+
+    //println!("{:?}", theta);
+
+    let xx = xy.slice(s![.., 0]);
+    for _i in 0..iterations{
+        let pre_des = x.dot(theta)-y;
+        let des_0 = alpha * (1.0/x_num_of_cols as f64) * &pre_des.sum();
+        let des_1 = alpha * (1.0/x_num_of_cols as f64) * (pre_des * xx).sum();
+    
+        theta[0] = theta[0] - des_0;
+        theta[1] = theta[1] - des_1;
+
+        //println!("{}", theta);
+    }
+
 }
--- machineLearning/week2/rust_ex1/src/main.rs
+++ machineLearning/week2/rust_ex1/src/main.rs
@@ -1,4 +1,4 @@
-use ndarray::{Array2, arr1};
+use ndarray::{Array2, arr1, Array1};
 use std::io::{stdin, Read};
 
 mod warm_up_exercise;
@@ -19,6 +19,8 @@ fn main() {
     
     // ======================= Part 2: Plotting =======================
 
+    println!("Plotting Data ...");
+
     let vec01 = arr1(&[10.0, 20.0, 3.0]);
     let vec02 = arr1(&[4.0, 5.0, 6.]);
     plot_data::plot_data_arr1(vec01, vec02, "test1.png");
@@ -31,7 +33,55 @@ fn main() {
 
     // =================== Part 3: Cost and Gradient descent ===================
 
-    let theta = arr1(&[-1.0, 2.0]);
+    let mut theta= Array1::<f64>::zeros(2);
 
-    linear_regression::compute_cost(&data, &theta);
+    // Some gradient descent settings
+    let iterations = 1500;
+    let alpha = 0.01;
+
+    println!("\nTesting the cost function ...");
+
+    // compute and display initial cost
+    let mut j = linear_regression::compute_cost(&data, &theta);
+    println!("With theta = [0 ; 0]\nCost computed = {:.2}", j);
+    println!("Expected cost value (approx) 32.07");
+
+    // further testing of the cost function
+    theta = arr1(&[-1.0, 2.0]);
+    j = linear_regression::compute_cost(&data, &theta);
+    println!("\nWith theta = [-1 ; 2]\nCost computed = {:.2}", j);
+    println!("Expected cost value (approx) 54.24");
+
+    print!("Program paused. Press enter to continue.\n");
+    stdin().read(&mut [0]).unwrap();
+
+    println!("\nRunning Gradient Descent ...");
+    // run gradient descent
+    theta = arr1(&[0.0, 0.0]);
+    linear_regression::gradient_descent(&data, &mut theta, alpha, iterations);
+
+    // print theta to screen
+    println!("Theta found by gradient descent:\n");
+    println!("{}", theta);
+    println!("Expected theta values (approx)\n");
+    println!(" [-3.6303,  1.1664]\n\n");
+
+    // Plot the linear fit
+    /*hold on; % keep previous plot visible
+    plot(X(:,2), X*theta, '-')
+    legend('Training data', 'Linear regression')
+    hold off % don't overlay any more plots on this figure
+
+    % Predict values for population sizes of 35,000 and 70,000
+    predict1 = [1, 3.5] *theta;
+    println!("For population = 35,000, we predict a profit of %f\n',...
+        predict1*10000);
+    predict2 = [1, 7] * theta;
+    println!("For population = 70,000, we predict a profit of %f\n',...
+        predict2*10000);
+
+    println!("Program paused. Press enter to continue.\n');
+    pause;*/
+
+    // ============= Part 4: Visualizing J(theta_0, theta_1) =============
 }
