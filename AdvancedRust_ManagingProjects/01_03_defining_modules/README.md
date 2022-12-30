23a50c4ec6f62231fbc18b8b58ce18a6727f8a07
xoffio <marquessricardo@gmail.com>
2022-12-30 11:05:42 -0500

    update class code

---
+++ AdvancedRust_ManagingProjects/01_03_defining_modules/README.md
@@ -0,0 +1,12 @@
+# 01.03 to 01.04
+
+In this package we will write the content of the videos 
+show the video `1.3 Defining Modules` and 
+
+`1.3 Defining Modules` inline modules
+`1.4 Absolute vs. relative path` absolute and relative paths
+`1.5 Public modules` privacy in rust
+
+## Inline
+
+## 
--- AdvancedRust_ManagingProjects/01_03_defining_modules/hello_world/src/main.rs
+++ AdvancedRust_ManagingProjects/01_03_defining_modules/hello_world/src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
-    println!("Hello, world!");
+    crate::hello::english();            // Hello - Absolute
+    crate::hello::spanish();            // Hola  - Absolute
+    hello::spanish();                   // Hola  - Relative
+    crate::hello::casual::english();    // Hi    - Absolute
+}
+
+// Defining inline module hello
+mod hello{
+    fn english(){
+        println!("Hello");
+        spanish();                      // Hola  - Relative
+        casual::english();              // Hi    - Relative
+    }
+
+    fn spanish(){
+        println!("Hola");
+    }
+
+    // Defining another module inside the module hello
+    mod casual{
+        fn english(){
+            println!("Hey");
+            crate::hello::spanish();    // Hola  - Absolute
+            super::spanish();           // Hola  - Relative
+        }
+    }
 }
