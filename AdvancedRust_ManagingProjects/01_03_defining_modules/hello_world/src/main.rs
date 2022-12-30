760a7286fb057d9ca8077dc93f42e3429902d24e
xoffio <marquessricardo@gmail.com>
2022-12-30 12:07:09 -0500

    Finishing code for videos 1.3 to 1.5

--- AdvancedRust_ManagingProjects/01_03_defining_modules/hello_world/src/main.rs
+++ AdvancedRust_ManagingProjects/01_03_defining_modules/hello_world/src/main.rs
@@ -1,13 +1,13 @@
 fn main() {
     crate::hello::english();            // Hello - Absolute
-    crate::hello::spanish();            // Hola  - Absolute
-    hello::spanish();                   // Hola  - Relative
-    crate::hello::casual::english();    // Hi    - Absolute
+    //crate::hello::spanish();            // Hola  - Absolute
+    //hello::spanish();                   // Hola  - Relative
+    //crate::hello::casual::english();    // Hi    - Absolute
 }
 
 // Defining inline module hello
 mod hello{
-    fn english(){
+    pub fn english(){
         println!("Hello");
         spanish();                      // Hola  - Relative
         casual::english();              // Hi    - Relative
@@ -18,8 +18,9 @@ mod hello{
     }
 
     // Defining another module inside the module hello
-    mod casual{
-        fn english(){
+    // You need to make this module public if you want access it from a higher level
+     mod casual{
+        pub fn english(){
             println!("Hey");
             crate::hello::spanish();    // Hola  - Absolute
             super::spanish();           // Hola  - Relative
