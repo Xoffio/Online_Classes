79afaa84d6f5c7641507fb9fd1318cfd0686c86c
xoffio <marquessricardo@gmail.com>
2022-12-30 21:24:09 -0500

    code for 01.06 updated

--- AdvancedRust_ManagingProjects/01_06_public_structs_and_enums/hello_mod/src/main.rs
+++ AdvancedRust_ManagingProjects/01_06_public_structs_and_enums/hello_mod/src/main.rs
@@ -1,23 +1,29 @@
 fn main() {
-    let rect = Rectangle::new(2.0 , 3.5);
+    let rect = shape::Rectangle::new(2.0 , 3.5);
     println!("rect area is {}", rect.get_area());
     println!("rect width is {}", rect.width);
 }
 
-struct Rectangle {
-    width: f64,
-    height: f64,
-}
-
-impl Rectangle {
-    fn new(width: f64, height: f64) -> Rectangle {
-        Rectangle {
-            width,
-            height
-        }
+mod shape {
+    // Struct needs to be public to be accessed
+    pub struct Rectangle {
+        // also each item in it
+        pub width: f64,
+        height: f64,
     }
-
-    fn get_area(&self) -> f64 {
-        self.width * self.height
+    
+    // imp does not need to be public
+    // but each item in it does.
+    impl Rectangle {
+        pub fn new(width: f64, height: f64) -> Rectangle {
+            Rectangle {
+                width,
+                height
+            }
+        }
+    
+        pub fn get_area(&self) -> f64 {
+            self.width * self.height
+        }
     }
 }
