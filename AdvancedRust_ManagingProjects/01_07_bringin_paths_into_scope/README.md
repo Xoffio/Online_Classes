520cbab2accc855e9b4f2508ab7fc201dde7a582
xoffio <marquessricardo@gmail.com>
2022-12-31 11:29:32 -0500

    Adding class 1.8

--- AdvancedRust_ManagingProjects/01_07_bringin_paths_into_scope/README.md
+++ AdvancedRust_ManagingProjects/01_07_bringin_paths_into_scope/README.md
@@ -1,8 +1,8 @@
-# 01.07
+# 01.07 to 01.08
 
 In this package we will write the content of the videos:
 
-## `1.7 Bringing paths into scope` inline modules
+## `1.7 Bringing paths into scope`
 
 Sometimes paths are vey long and verbose. To simplify the paths in our code we can use the `use` statement to bring a path into the local scope. This is very useful when you need to call a function from a module multiple times. 
 
@@ -26,4 +26,17 @@ use crate::greeting::casual;
 
 // Even better 
 use crate::greeting::{formal, casual};
+```
+
+## `1.8 Using external crates`
+
+To bring multiple items with the same name into scope you can use `as` to change th name of it.
+```rust
+use crate::greeting::formal::spanish as f_spanish;
+use crate::greeting::casual::spanish as c_spanish;
+```
+
+to bring multiple items from a module you can use the glob operator `*`
+```rust
+use rand::prelude::*;
 ```
--- AdvancedRust_ManagingProjects/01_07_bringin_paths_into_scope/hello_world/src/main.rs
+++ AdvancedRust_ManagingProjects/01_07_bringin_paths_into_scope/hello_world/src/main.rs
@@ -3,6 +3,15 @@
 //use crate::greeting::casual;
 use crate::greeting::{formal, casual};
 
+// You can use `as` to change the name of an item brought into scope
+// this way you are able to bring items with the same name without a problem.
+use crate::greeting::formal::spanish as f_spanish;
+use crate::greeting::casual::spanish as c_spanish;
+
+// Bringing all items in the prelude module of the `rand` crate
+// the prelude items are the most used items.
+use rand::prelude::*;
+
 // You can also be more specific
 use crate::greeting::formal::spanish;
 
@@ -10,7 +19,15 @@ fn main() {
     crate::greeting::formal::english();
 
     formal::english();
+    casual::english();
     spanish();
+
+    println!("--- Using `use` and `as` ---");
+    f_spanish();
+    c_spanish();
+
+    // using the function `random` from the rand prelude library 
+    println!("char: {}", random::<u8>());
 }
 
 // Defining inline module greeting
