5a58c4d68ff1900df9016aa70d800d26fe7fd4e9
xoffio <marquessricardo@gmail.com>
2023-01-14 09:37:08 -0500

    Initial code for video 02.07

---
+++ AdvancedRust_ManagingProjects/02_07_unit_tests/math_funcs/src/lib.rs
@@ -0,0 +1,20 @@
+/*****************************
+ * Library of Math Functions *
+ *****************************/
+pub mod add_ops; // code in add_ops.rs
+pub mod mul_ops; // code in mul_ops.rs
+
+// Only run when `cargo test` is ran
+#[cfg(test)]
+mod tests {
+	// Bring all upper functions into scope.
+	use super::*;
+
+	#[test]
+	fn ut_add_and_mul_ops() { // test involving both modules
+		let x = add_ops::add_two(3.14159, 2.71828); // 5.85987
+		let y = mul_ops::mul_three(3.0, 4.0, 5.0); // 30.0
+		let result = add_ops::add_three(x, y, 123.4); // 159.25987
+		assert_eq!(result, 189.25987);
+	}
+}
