057e394a01ea5d542be8b199666fda897a27327f
xoffio <marquessricardo@gmail.com>
2023-01-16 14:02:59 -0500

    print more values

--- AdvancedRust_ManagingProjects/01_10_challenge_modules/challenge_modules/src/main.rs
+++ AdvancedRust_ManagingProjects/01_10_challenge_modules/challenge_modules/src/main.rs
@@ -5,9 +5,14 @@ use crate::shapes::feature::Feature;
 fn main() {
     let rect = Rectangle::new(1.0, 2.0);
     let area = rect.get_feature(Feature::Area);
+    let rect_perimeter = rect.get_feature(Feature::Perimeter);
     println!("rect area is {}", area);
+    println!("rect perimeter is {}", rect_perimeter);
 
     let circ = Circle::new(3.0);
     let perimeter = circ.get_feature(Feature::Perimeter);
+    let c_area = circ.get_feature(Feature::Area);
+
     println!("circ perimeter is {}", perimeter);
+    println!("circ area is {}", c_area);
 }
